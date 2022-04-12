eca5739d4fb46a177b134f1f3f997b04aa5a4e3b
Danil <96177948+0x7o@users.noreply.github.com>
2022-04-12 20:04:57 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,4 +5,5 @@ setup(name='ficaptcha',
       description='Gaussian and Binomial distributions',
       packages=['ficaptcha'],
       author_email='me@0x7o.link',
+      install_requires=['numpy', 'pillow'],
       zip_safe=False)

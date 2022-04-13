7cc4a1c53b3cb9fa4d5f59383ea595aa1b7db1ca
Danil <96177948+0x7o@users.noreply.github.com>
2022-04-13 18:46:24 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,23 @@
-from setuptools import setup
+import setuptools
 
-setup(name='ficaptcha',
-      version='1.0',
-      description='Simple captcha generation',
-      packages=['ficaptcha'],
-      author_email='me@0x7o.link',
-      install_requires=['numpy', 'pillow'],
-      zip_safe=False)
+setuptools.setup(
+	name="ficaptcha",
+	version="1.0.0",
+	author="Danil Kononyuk",
+	author_email="me@0x7o.link",
+	description="Simple captcha",
+	long_description="""# fiCaptcha
+Module for image-captcha generation
+
+## Build from source
+```
+git clone https://github.com/0x7o/fiCaptcha
+cd fiCaptcha
+pip install .
+```""",
+	long_description_content_type="text/markdown",
+	url="https://github.com/0x7o/fiCaptcha",
+	packages=setuptools.find_packages(),
+	classifiers=[],
+	python_requires='>=3.6',
+)

f3bbe7aa31a93ef694df43baf7454dc7ba4214e5
Danil <96177948+0x7o@users.noreply.github.com>
2022-04-17 13:27:16 +0800

    audio

--- test.py
+++ test.py
@@ -2,4 +2,6 @@ import time
 import random
 from ficaptcha.audio import Captcha
 
-c = Captcha(audio_synthesis=True, audio_dir="a")
+c = Captcha(audio_synthesis=False, audio_dir="audio", count=5, noise_dir="noise")
+c.generate()
+c.save("1.wav", "wav")
